d43cd92abfc471c012a52dc2a4e93b8afe43e8a6
Manuel Zapata <manuelricardozapata@gmail.com>
2012-04-26 00:51:05 -0500

    Created classes and XAML for pages and services. Added functionality to provide basic navigation between the pages.

---
+++ apps/WindowsPhone/MIO/MIO/Pages/Home.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace MIO.Pages
+{
+    public partial class HomePage : PhoneApplicationPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateToInnerPage(string pageName)
+        {
+            if (!string.IsNullOrEmpty(pageName))
+            {
+                string format = "/{0}/{1}";
+                string uri = string.Format(format, Util.Constants.PagesNamespaceName, pageName);
+                NavigationService.Navigate(new Uri(uri, UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void btnMainRoutes_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToInnerPage("MainRoutes.xaml");
+        }
+
+        private void btnAdditionalRoutes_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToInnerPage("AdditionalRoutes.xaml");
+        }
+
+        private void btnNearbyStations_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToInnerPage("NearbyStations.xaml");
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToInnerPage("About.xaml");
+        }
+    }
+}
--- apps/WindowsPhone/MIO/MIO/Pages/MainRoutes.xaml.cs
+++ apps/WindowsPhone/MIO/MIO/Pages/MainRoutes.xaml.cs
@@ -11,12 +11,11 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
-namespace MIO
+namespace MIO.Pages
 {
-    public partial class MainPage : PhoneApplicationPage
+    public partial class MainRoutes : PhoneApplicationPage
     {
-        // Constructor
-        public MainPage()
+        public MainRoutes()
         {
             InitializeComponent();
         }
